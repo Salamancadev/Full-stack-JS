bab2f45ca067990972975625cd62cca08b24aa1b
Salamancadev <sansalrus@gmail.com>
2024-12-17 14:59:50 -0500

    Entedimos que son, como funcionan y como crear variables en JS

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/2-Variables.js
@@ -0,0 +1,6 @@
+//Para crear nuestra variable que funciona como caja virtual utilizamos let
+//Los nombres tienen que ser descriptivos para que sea más facil su comprensión
+
+let tanquesAlemamia = 'Tiger II'
+console.log(tanquesAlemamia)
+
