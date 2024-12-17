b6cf05b1a1cd61af545a5f2501c526ddac5e40d4
Salamancadev <sansalrus@gmail.com>
2024-12-17 15:00:38 -0500

    Vimos los 10 diferentes tipos de datos que hay en JS, como se clasifican y como se escriben dentro de JS

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/3-Tipos_de_Datos.js
@@ -0,0 +1,27 @@
+//Vamos a ver los tipos de datos en JS
+
+//PRIMITIVOS
+
+//String para cadenas de caracteres/textos
+let nombre = 'Tere';
+
+//Number para valores númericos 
+let edad = 15;
+
+//Boolean para afirmar o negar (True/False)
+let esMayor = true;
+
+//Null para representar ausencia de algun valor
+let noHayValor = null;
+
+//Undefined variable declarada pero sin valor asignado
+let noDefinido = undefined;
+
+//Symbol para indicar un valor unico
+let simboloUnico = Symbol('único')
+
+//Bigint para variables númericas enormes
+let numeroGrande = 2n
+
+//COMPLEJOS
+
