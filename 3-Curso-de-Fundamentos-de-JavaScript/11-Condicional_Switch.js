ea91cec3be222233a598859d66bc2e81224d5420
Salamancadev <sansalrus@gmail.com>
2024-12-19 17:39:28 -0500

    Vimos el condicional swicht y como manejarlo

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/11-Condicional_Switch.js
@@ -0,0 +1,13 @@
+switch (expresion) {
+    case valor1:
+        // Código a ejecutar
+        break;
+    case valor1:
+        // Código a ejecutar
+        break;
+    case valor1:
+        // Código a ejecutar
+        break;  
+    default:
+         // Código a ejecutar
+}
