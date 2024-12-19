caa8c76be26ef28c75d582fe628cee6030f8c654
Salamancadev <sansalrus@gmail.com>
2024-12-19 15:11:08 -0500

    Vimos los de operadores de logicos, como utilizarlo e interpretarlos

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/8-Operadores_Logicos.js
@@ -0,0 +1,22 @@
+// Operadores Lógicos
+
+// && AND: Si este valor y este valor hacen algo pasa esto
+// || OR:  Si este valor A o este valor B cumplen hacen algo
+// !  NOT: Negación, invierte el valor d ela afirmacion
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+let and = a == b && a === c;
+console.log (and);
+console.log (typeof and)
+
+let or = a != b || a === c;
+console.log (or);
+console.log (typeof or)
+
+let not = !(a === c );
+console.log (not);
+console.log (typeof not)
+//Arroja true por que invierte el false
