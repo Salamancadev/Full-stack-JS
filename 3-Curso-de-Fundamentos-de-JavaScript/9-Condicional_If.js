1d1031112549b40cb99f53aaa25f96ff8ea5d6d4
Salamancadev <sansalrus@gmail.com>
2024-12-19 17:01:16 -0500

    Adicion la clase de condicional IF

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/9-Condicional_If.js
@@ -0,0 +1,9 @@
+let edad = "2"
+
+if ( edad === "18") {
+    console.log ("Usted es cumple la mayoria de edad")
+} else if ( edad > 18) {
+    console.log ("Usted es mayor de 18 años")
+} else {
+    console.log ("Usted no es mayor de edad")
+}
