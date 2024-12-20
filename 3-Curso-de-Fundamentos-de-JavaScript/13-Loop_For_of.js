d12374e43c96f99775a63ea8aea5028fe7b1aa0b
Salamancadev <sansalrus@gmail.com>
2024-12-20 11:54:42 -0500

    Vimos el condicional for of y como manejarlo

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/13-Loop_For_of.js
@@ -0,0 +1,14 @@
+/*
+For of: Forma de iterar elementos como arrays o strings
+for of arrays, strings [Algo]
+
+for (varaible of objeto) {
+    código
+}
+*/
+
+let canasta = ["manzana", "banano", "pera", "granadilla"]
+
+for ( fruta of canasta) {
+    console.log(fruta)
+}
