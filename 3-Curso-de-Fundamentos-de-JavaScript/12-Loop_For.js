5f9285f707e739e978fd88ba00f1f12e5ae739bc
Salamancadev <sansalrus@gmail.com>
2024-12-20 09:24:54 -0500

    Vimos el loop for y como manejarlo

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/12-Loop_For.js
@@ -0,0 +1,10 @@
+/*Loop for
+for (varaible; condicion; incremeto) {
+codio a ejecutar
+}*/ 
+
+let lista = ["manzana", "banano", "pera", "granadilla"]
+
+for (let i = 0; i < lista.length; i++ ) {
+    console.log(lista[i]);
+}
