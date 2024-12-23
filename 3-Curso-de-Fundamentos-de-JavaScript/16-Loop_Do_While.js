52fbd32fc7c229f7b977f628019cf82f7a94015e
Salamancadev <sansalrus@gmail.com>
2024-12-23 10:42:24 -0500

    Comprendimos el manejo del metodo Do While, y en que se diferencia con while

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/16-Loop_Do_While.js
@@ -0,0 +1,15 @@
+/*
+Metodo que nos ayuda tambien con ciclos
+
+do {
+    código
+} while (condicion)
+
+*/
+
+let contador = 0;
+
+do {
+    console.log(contador)
+    contador++
+} while(contador <10)
