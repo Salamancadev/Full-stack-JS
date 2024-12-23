28ec8d0496d1dc0b2d8b7f4066ffff3b947f3226
Salamancadev <sansalrus@gmail.com>
2024-12-23 10:27:54 -0500

    Entendimos el condicional while y como manejarlo

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/15-Loop_While.js
@@ -0,0 +1,16 @@
+/*
+While es un ciclo/bucle que nos va a ayudar a iterar
+
+while (condicion) {
+    codigo
+}
+
+Hay que tener cuidado de no caer en un loop infinito
+*/
+
+let contador = 0
+
+while (contador< 60){
+    console.log(contador)
+    contador++;
+}
