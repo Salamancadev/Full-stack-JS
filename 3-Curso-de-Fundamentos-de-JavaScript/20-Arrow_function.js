7eb1bfde7d4b87a582311ecb356da396f08f6775
Salamancadev <sansalrus@gmail.com>
2024-12-24 17:24:21 -0500

    Vimos diferentes sintaxis de funciones

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/20-Arrow_function.js
@@ -0,0 +1,27 @@
+const greeting = function (name) {
+    return `Hi, ${name}`
+}
+
+//Arrow function - explicit return
+
+const newGreeting = (name) => {
+    return `Hi, ${name}`
+}
+
+//Arrow function - implicit return
+
+const newGreetingImplicit = name => `Hi, ${name}`
+const newGreetingImplicitWithTwoParameters = (name, lastname) => `Hi, I'm ${name} ${lastname}`
+
+//Lexical Binding
+const fictionalCharacter = {
+    name: "Uncle Ben",
+    messageWithTraditionalFunction: function (message) {
+        console.log(`${this.name} says: ${message}`)
+    },
+    messageWithArrowFunction: (message) => {
+        console.log(`${this.name} says: ${message}`)
+    }
+}
+fictionalCharacter.messageWithTraditionalFunction('With great power comes great responsability.')
+fictionalCharacter.messageWithArrowFunction('Beware of Doctor Octupus.')
