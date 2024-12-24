e4649071433b0cd460e8b42e6396a6e10201a00d
Salamancadev <sansalrus@gmail.com>
2024-12-23 19:02:11 -0500

    Vimos como utilizar las funciones de diferentes maneras, ademas de comprender la diferencia entre una funcion y un metodo

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/18-Funciones_vs_Metodos.js
@@ -0,0 +1,49 @@
+//Capacidades que tienen las funciones al igual que otros objetos 
+
+//1. Pasar funciones como argumentos ->call back
+
+function a () {}
+function b (a) {}
+b(a)
+
+//Retornar funciones 
+
+function a () {
+    function b () {}
+    return b
+}
+
+//Asignar funciones a variables -> Expresión de función
+
+const a = function() {}
+
+//Tener propiedades y métodos
+
+function a () {}
+const obj = {}
+a.call(pbj)
+
+//Anidar funciones  -> Nested functions
+
+
+function a () {
+    function b () {
+        function c () {
+
+        }
+        c ()
+    }
+    b()   
+}
+a()
+
+// Es posible almacenar funciones en objetos?
+
+const rocket = {
+    name: 'Falcon 9',
+    launchMessage: function launchMessage () {
+        console.log ('🔥')
+    }
+}
+
+rocket.launchMessage()
