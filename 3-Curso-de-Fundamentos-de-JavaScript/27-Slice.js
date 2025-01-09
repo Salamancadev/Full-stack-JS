0119b734770085fca4997242cf42aa3d7cc9ea54
Salamancadev <sansalrus@gmail.com>
2025-01-09 18:31:19 -0500

    Vimos el metodo de slice() para los arrays

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/27-Slice.js
@@ -0,0 +1,13 @@
+//Slice()
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
+
+console.log(animals.slice(2));
+console.log(animals.slice(2, 4));
+console.log(animals.slice(1, 5));
+console.log(animals.slice(-2));
+console.log(animals.slice(2, -1));
+console.log(animals.slice);
+
+
+
