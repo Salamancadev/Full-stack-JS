190d938b1346e28902a1e496170cd3c888b1c64b
Salamancadev <sansalrus@gmail.com>
2025-01-11 15:57:52 -0500

    Vimos la practica sobre herencia de clases contructoras

---
+++ 3-Curso-de-Fundamentos-de-JavaScript/33-Herencia.js
@@ -0,0 +1,34 @@
+//Practica del concepto de prototipos y herencias.
+//La propiedad prototipo solo existe en objetos (Clases y Funciones constructoras)
+
+class Animal {
+    constructor(nombre, tipo){
+        this.nombre = nombre
+        this.tipo = tipo
+    }
+
+    emitirSonido(){
+        console.log("El animal emite un sonido");
+    }
+}
+
+class Perro extends Animal{
+    constructor(nombre, tipo, raza){
+        super(nombre, tipo)
+        this.raza = raza;
+    }
+
+    emitirSonido(){
+        console.log("El perro ladra");
+    }
+
+    correr(){
+        console.log(`${this.nombre} corre alegremente`);
+    }
+}
+
+const perro1 = new Perro("Bobby", "Perro", "Pug");
+
+console.log(perro1);
+perro1.correr();
+perro1.emitirSonido();
