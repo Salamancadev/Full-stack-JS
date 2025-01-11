c67129138ccaf43fe3231219d7ecda1533f1fb75
Salamancadev <sansalrus@gmail.com>
2025-01-11 17:01:44 -0500

    Vimos como es que funciona la cadena de prototipado con JS con las clases y sus instancias

--- 3-Curso-de-Fundamentos-de-JavaScript/33-Herencia.js
+++ 3-Curso-de-Fundamentos-de-JavaScript/33-Herencia.js
@@ -32,3 +32,14 @@ const perro1 = new Perro("Bobby", "Perro", "Pug");
 console.log(perro1);
 perro1.correr();
 perro1.emitirSonido();
+
+//Nuevo metodo para instancia de perro1
+perro1.nuevoMetodo = function() {
+    console.log("Este es un metodo");
+};
+
+//Los nuevos metodos agregados por prototype solo se pueden agregar a las clases/funciones constructoras, no a las instancias
+//Esto se logra gracias a la cadena de prototipado
+Perro.prototype.segundoMetodo = function (){
+    console.log("Es otro nuevo metodo");
+};
