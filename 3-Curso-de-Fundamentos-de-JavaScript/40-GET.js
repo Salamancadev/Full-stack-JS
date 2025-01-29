644f6c64e6cb36c8d4e7f5be0b79cccd52457443
Salamancadev <sansalrus@gmail.com>
2025-01-29 16:57:17 -0500

    En esta parte del curso aprendimos como es llamar información de un API con el metodo Get con JS

--- 3-Curso-de-Fundamentos-de-JavaScript/40-GET.js
+++ 3-Curso-de-Fundamentos-de-JavaScript/40-GET.js
@@ -1,22 +1,55 @@
-//Clase metodo GET: Vamos a ver como hacer una petición y como interactuar con ella
+//Clase metodo GET: Vamos a ver como hacer una petición y como 
+// interactuar con ella y como escalar con mas las de mas peticiones
 
-function sendHTTPRequest(method, url, data){
-    return fetch(url,{
-        method: method,
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    } ).then((response) => {
-        return response.json ();
-    });
+//Vamos a empezar a crear una petición y a como interactuar con esa petición.
+
+//Empezamos con una función, llevara tres parametros
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("#posts-container");
+
+function sendHTTPRequest(method, url, data) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
 }
 
-async function fetchPosts() {
-    const responseData = await 
-    sendHTTPRequest("GET", "https://jsonplaceholder.typicode.com/posts");
-    console.log(responseData);
-    const listOfPosts = responseData;
-    
+async function fecthPosts() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(responseData);
+  const listOfPosts = responseData;
+
+  for (const post of listOfPosts) {
+    const postContainer = document.createElement("article");
+    postContainer.id = post.id;
+    postContainer.classList.add("post-item");
+
+    const title = document.createElement("h2");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    const button = document.createElement("button");
+    button.textContent = "DELETE Content";
+
+    postContainer.append(title);
+    postContainer.append(body);
+    postContainer.append(button);
+
+    listElement.append(postContainer);
+  }
 }
 
+fetchButton.addEventListener("click", fecthPosts);
